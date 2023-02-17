72068eab6c6b15b095d086645f1ff4f60dcc966a
Thibault Dorn <thibault.dorn@grenoble-inp.org>
2023-02-17 19:02:19 +0100

    spotify iframe test

--- src/frontend/base/index.js
+++ src/frontend/base/index.js
@@ -1,1 +1,8 @@
-console.log("hello");
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+  let element = document.getElementById("embed-iframe");
+  let options = {
+    uri: "spotify:track:67PWiuodhQW3lAANwQpYjY",
+  };
+  let callback = (EmbedController) => {};
+  IFrameAPI.createController(element, options, callback);
+};
