fc869853634d6634bf01f89a3f632b87244b5116
tibo <dornthibault@gmail.com>
2023-03-11 13:14:59 +0100

    codeerror + users api

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const logger = require('./logger');
 const app = express();
 app.use(logger.dev, logger.combined);
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log('Time:', Date.now());
   console.log('Request URL:', req.originalUrl);
-  console.log('Request Type:', req.method);
+  console.log('Req@uest Type:', req.method);
   next();
 });
 
@@ -19,5 +20,9 @@ app.use('/', require('./routes/router.js'));
 app.use('*', (req, res) => {
   res.status(404).json({ status: false, message: 'Endpoint Not Found' });
 });
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send('Why would you broke the server ?');
+// });
 
 module.exports = app;
