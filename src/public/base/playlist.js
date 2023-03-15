dc60b5fa34d10a22cc71b6373b8400310493190c
tibo <dornthibault@gmail.com>
2023-03-15 23:49:45 +0100

    Refactor + refreshPlaylist init

--- src/public/base/playlist.js
+++ src/public/base/playlist.js
@@ -1,24 +1,31 @@
 import { toggleMute } from "./lib.js";
 import { toggleSidebar } from "./lib.js";
 const backend = "localhost:3000";
+const prisma = new PrismaClient()
 
-function startUp() {
+funciton init() {
   const menuIcon = document.querySelector("#menu-icon-bg");
   const muteSpan = document.getElementById("mute");
-  const audio = new Audio("../music/bee-gees-stayin-alive.wav");
   const signinBtn = document.getElementById("signinBtn");
   const searchInput = document.getElementById("search");
+
   menuIcon.addEventListener("click", toggleSidebar);
   muteSpan.addEventListener("click", () => toggleMute(audio));
-  // let audio = new Audio("../music/bee-gees-stayin-alive.mp3"); //try catch
-  audio.preload = "auto";
-  audio.volume = 0.1;
-  audio.loop = true;
   signinBtn.addEventListener(
     "click",
     () => (window.location.href = "signin.html")
   );
   searchInput.addEventListener("keypress", runSearch);
+}
+
+function startUp() {
+  init()
+  refreshPlaylist()
+  const audio = new Audio("../music/bee-gees-stayin-alive.wav");
+  audio.preload = "auto";
+  audio.volume = 0.1;
+  audio.loop = true;
+
   //TODO ajouter une chanson si elle n'est pas déjà dans la playlist
   //TODO voter une chanson
   //TODO dévoter une chanson
@@ -26,12 +33,26 @@ function startUp() {
   //TODO Chercher un nom, le résultat s'affiche et la chanson s'ajoute quand on clique
   //TODO Chercher via spotify (5 à chaque lettre)
 }
-
+async function refreshPlaylist(playlistId) {
+  const playlistDiv = document.getElementById("div.list")
+  const results = await prisma.playlistSong.findMany({
+    where: {
+      playlistId: playlistId
+    }
+  })
+  if (results != null) {
+    results.forEach((s) =>
+    {
+      console.log('add div');
+    })
+  }
+}
 function runSearch(e) {
   if (e.key === "Enter") {
     const endPoint = "/songs";
     const url = new URL(backend + endPoint);
-    fetch(url, { method: "GET" });
+    // fetch(url, { method: "GET" });
+    console.log("Requête spotify :", url)
   }
 }
 
