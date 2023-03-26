3acd27866f06826df476b40e957997e568aad0ad
tibo <dornthibault@gmail.com>
2023-03-27 00:25:06 +0200

    Swagger syntax errors

--- src/controllers/playlistSongs.js
+++ src/controllers/playlistSongs.js
@@ -1,15 +1,33 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const status = require("http-status");
-const has = require("has-keys");
-const CodeError = require("../CodeError");
+const status = require('http-status');
+const has = require('has-keys');
+const CodeError = require('../CodeError');
 
 //TODO doc api
 module.exports = {
   async searchSong(req, res) {
+    /*
+    #swagger.tags = ['Playlist song']
+    #swagger.summary = 'Find a song in a playlist.'
+    #swagger.parameters['playlistId'] = {
+        in: 'path',
+        description: 'Id of a playlist',
+        required: true,
+        type: 'integer',
+        schema: { $playlistId: 4 }
+    }
+    #swagger.parameters['songId'] = {
+        in: 'path',
+        description: 'Id of a song.',
+        required: true,
+        type: 'integer',
+        schema: { $songId: 13 }
+    }
+    */
     //TODO id validation
-    if (!has(req.params, ["playlistId", "songId"]))
-      throw new CodeError("Missing parameters", 400);
+    if (!has(req.params, ['playlistId', 'songId']))
+      throw new CodeError('Missing parameters', 400);
     const playlistId = parseInt(req.params.playlistId);
     const songId = parseInt(req.params.songId);
     const playlistSong = await prisma.playlistSong.findFirst({
@@ -20,19 +38,53 @@ module.exports = {
     });
     if (playlistSong !== null) {
       res.status(200).json({
-        message: "The song was found !",
+        message: 'The song was found !',
         playlistSong,
       });
     } else {
       res.status(404).json({
-        message: "Song not found",
+        message: 'Song not found',
       });
     }
+    /*
+      #swagger.responses[200] = {
+          description: 'Song found.',
+          schema: {
+            $ref: '#/definitions/playlistSong
+          }
+      }
+      #swagger.responses[404] = {
+          description: 'Song not found.',
+          schema: {
+            message: 'Song not found',
+          }
+      }
+      */
   },
   async addSong(req, res) {
+    /*
+    #swagger.tags = ['Playlist song']
+    #swagger.summary = 'Add a song in a playlist.'
+    #swagger.parameters['playlistId'] = {
+        in: 'path',
+        description: 'Id of a playlist',
+        required: true,
+        type: 'integer',
+    }
+    #swagger.parameters['obj'] = {
+        in: 'body',
+        description: 'Song ID, user ID and votesNB.',
+        required: true,
+        schema: {
+            $songId: 13,
+            votesNb: 3,
+            submitterId: 7
+        }
+    }
+    */
     //TODO id validation
-    if (!has(req.body, ["songId"]))
-      throw new CodeError("Missing parameter", 400);
+    if (!has(req.body, ['songId']))
+      throw new CodeError('Missing parameter', 400);
     //TODO Email validation
     const playlistId = parseInt(req.params.playlistId);
     const songId = parseInt(req.body.songId);
@@ -43,13 +95,13 @@ module.exports = {
       },
     });
     console.log(
-      "playlistid: ",
+      'playlistid: ',
       playlistId,
-      "songId:",
+      'songId:',
       songId,
-      "votesNb: ",
+      'votesNb: ',
       req.body.votesNb,
-      "submitterID",
+      'submitterID',
       req.body.submitterId
     );
     if (searchedSong === null) {
@@ -62,16 +114,48 @@ module.exports = {
         },
       });
       res.status(201).json({
-        message: "The song was added !",
+        message: 'The song was added !',
         song,
       });
     } else {
       res.status(400).json({
-        message: "The song already exists",
+        message: 'The song already exists',
       });
     }
+    /*
+      #swagger.responses[201] = {
+          description: 'Song added.',
+          schema: {
+              message: 'The song was added",
+              $ref: '#/definitions/playlistSong
+          }
+      }
+      #swagger.responses[400] = {
+          description: 'Song already exists.',
+          schema: {
+              message: 'The song already exists'
+          }
+      }
+      */
   },
   async removeSong(req, res) {
+    /*
+    #swagger.tags = ['Playlist song']
+    #swagger.summary = 'Remove a song in a playlist.'
+    #swagger.parameters['playlistId'] = {
+        in: 'path',
+        description: 'Id of a playlist',
+        required: true,
+        type: 'integer',
+    }
+    #swagger.parameters['songId'] = {
+        in: 'path',
+        description: 'Id of a song.',
+        required: true,
+        type: 'integer',
+    }
+    */
+
     //TODO id validation
     //TODO Email validation
     const playlistId = parseInt(req.params.playlistId);
@@ -86,11 +170,44 @@ module.exports = {
     });
     // const message = vote === null ? 'Vote created' + vote : '';
     res.status(200).json({
-      message: "Song deleted",
+      message: 'Song deleted',
       song,
     });
+    /*
+    #swagger.responses[200] = {
+        description: 'Song deleted.',
+        schema: {
+            message: 'Song deleted",
+            $ref: '#/definitions/playlistSong
+        }
+    }
+    */
   },
   async editSong(req, res) {
+    /*
+    #swagger.tags = ['Playlist song']
+    #swagger.summary = 'Update a song in a playlist.'
+    #swagger.parameters['playlistId'] = {
+        in: 'path',
+        description: 'Id of a playlist',
+        required: true,
+        type: 'integer',
+    }
+    #swagger.parameters['songId'] = {
+        in: 'path',
+        description: 'Id of a song.',
+        required: true,
+        type: 'integer',
+    }
+    #swagger.parameters['obj'] = {
+        in: 'body',
+        description: 'Total of song's votes and submitter.',
+        schema: {
+            votesNb: 13,
+            submitterId: 13
+        }
+    }
+    */
     const playlistId = req.params.playlistId;
     const songId = req.params.songId;
     const song = await prisma.playlistSong.update({
@@ -106,11 +223,27 @@ module.exports = {
       },
     });
     res.status(200).json({
-      message: "Song updated",
+      message: 'Song updated',
       song,
     });
+    // #swagger.responses[200] = {
+    //     description: 'Song updated.',
+    //     schema: {
+    //         message: 'Song updated",
+    //         $ref: '#/definitions/playlistSong
+    //     }
+    // }
   },
   async getSongs(req, res) {
+    // #swagger.tags = ['Playlist song']
+    // #swagger.summary = 'Get all songs in a playlist.'
+    // #swagger.parameters['playlistId'] = {
+    //     in: 'path',
+    //     description: 'Id of a playlist',
+    //     required: true,
+    //     type: 'integer',
+    // }
+
     const playlistId = parseInt(req.params.playlistId);
     const results = await prisma.playlistSong.findMany({
       where: {
@@ -120,12 +253,20 @@ module.exports = {
         song: true,
       },
       orderBy: {
-        votesNb: "desc",
+        votesNb: 'desc',
       },
     });
     res.status(200).json({
-      message: "Song(s) found !",
+      message: 'Song(s) found !',
       results,
     });
+    // #swagger.responses[200] = {
+    //     description: 'Song(s) found.',
+    //     schema: {
+    //         message: 'Song(s) found !",
+    //     }
+    // }
+
+    //TODO finish results format
   },
 };
--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -1,4 +1,4 @@
-const swaggerAutogen = require('swagger-autogen')()
+const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     version: '1.0.0',
@@ -7,15 +7,24 @@ const doc = {
   },
   host: 'localhost:3000/api/v1',
   schemes: ['http'],
+  definitions: {
+    playlistSong: {
+      playlistId: 1,
+      songId: 14,
+      votesNb: 53,
+      submitterId: 2,
+      createdAt: '2023-03-25 13:20:24.579',
+    },
+  },
 };
-const outputFile = 'swagger_output.json'
+const outputFile = 'swagger_output.json';
 const endpointsFiles = [
   'src/routes/users.js',
   'src/routes/router.js',
   'src/routes/songs.js',
   'src/routes/votes.js',
   'src/routes/playlistSongs.js',
-  'src/routes/playlists.js'
-]
+  'src/routes/playlists.js',
+];
 
-swaggerAutogen(outputFile, endpointsFiles, doc)
+swaggerAutogen(outputFile, endpointsFiles, doc);
