2e47a4c16cd84ab6d18569f1fefa05db49323e75
tibo <dornthibault@gmail.com>
2023-04-12 23:57:29 +0200

    Add password hash

--- src/public/base/signin.js
+++ src/public/base/signin.js
@@ -1,5 +1,6 @@
-import { toggleMute } from './lib.js';
-import { toggleSidebar } from './lib.js';
+import { toggleMute, toggleSidebar } from './lib.js';
+const backend = 'http://localhost:3000';
+
 function startUp() {
   const menuIcon = document.querySelector('#menu-icon-bg');
   const muteSpan = document.getElementById('mute');
@@ -14,14 +15,65 @@ function startUp() {
   audio.loop = true;
   signinBtn.addEventListener(
     'click',
-    () => (window.location.href = 'playlist.html')
+    // () => (window.location.href = 'playlist.html'
+    sendCredentials
+    // )
   );
+  console.log('Listener added');
   registerBtn.addEventListener(
     'click',
     () => (window.location.href = 'signin.html')
   );
 }
+
+async function sendCredentials() {
+  const signDiv = document.querySelector('div.sign');
+  const login = signDiv.getElementsByTagName('input')[0].value;
+  const password = signDiv.getElementsByTagName('input')[1].value;
+  const mail = signDiv.getElementsByTagName('input')[2].value;
+  const hashed = await hashPass(password);
+  console.log('Password:', password);
+  console.log('Hashed:', hashed);
+  const url = new URL(backend + '/auth');
+  const data = {
+    login: login,
+    password: hashed,
+    mail: mail,
+  };
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'x-access-token': TOKEN,
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json)
+    .then((json) => {
+      console.log(json);
+      if (json.status === 200) {
+        console.log('Token: ', json.token);
+      }
+      if (json.status === 403) {
+        console.log('No token', json.message);
+      }
+    })
+    .catch((e) => console.log(e));
+}
+
+async function hashPass(password) {
+  console.log('hash start');
+  const hashDigest = await crypto.subtle.digest(
+    'SHA-256',
+    new TextEncoder().encode(password)
+  );
+  const hashArray = Array.from(new Uint8Array(hashDigest));
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, '0'))
+    .join('');
+  return hashHex;
+}
 //TODO send post request using encrypted password
 //TODO Store received token in cookie
+//TODO disclaimer valeur du mot de passe
 
 window.addEventListener('load', startUp);
