579368c058db3d8b808f1d59d917f73e223e5da1
tibo <dornthibault@gmail.com>
2023-04-19 20:43:15 +0200

    Test passenger

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,9 +1,17 @@
 // require('mandatoryenv').load(['PORT']);
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 const { PORT } = process.env;
 
 // Instantiate an Express Application
-const app = require('./app');
+const app = require("./app");
 // Open Server on selected Port
-app.listen(PORT, () => console.info('Server listening on port ', PORT));
+if (typeof PhusionPassenger !== "undefined") {
+  PhusionPassenger.configure({ autoInstall: false });
+}
+
+if (typeof PhusionPassenger !== "undefined") {
+  app.listen("passenger");
+} else {
+  app.listen(3000);
+}
