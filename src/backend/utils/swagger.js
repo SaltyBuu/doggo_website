fc17f43fb3c8d95d7500729377c3ab3d580fc44b
tibo <dornthibault@gmail.com>
2023-04-20 18:43:25 +0200

    Fix backend paths

--- src/backend/utils/swagger.js
+++ src/backend/utils/swagger.js
@@ -1,41 +1,41 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")();
 const doc = {
   info: {
-    version: '1.0.0',
-    title: 'REST API',
-    description: '',
+    version: "1.0.0",
+    title: "REST API",
+    description: "",
   },
-  host: 'localhost:3000/api/v1',
-  schemes: ['http'],
+  host: "localhost:3000/api/v1",
+  schemes: ["http"],
   definitions: {
     playlistSong: {
       playlistId: 1,
       songId: 14,
       votesNb: 53,
       submitterId: 2,
-      createdAt: '2023-03-25T13:20:24.579Z',
+      createdAt: "2023-03-25T13:20:24.579Z",
     },
     vote: {
       playlistId: 2,
       songId: 4,
       userid: 3,
-      voteDate: '2023-03-27T21:23:57.672Z',
+      voteDate: "2023-03-27T21:23:57.672Z",
     },
     user: {
       id: 3,
-      login: 'Alfredus',
-      password: 'oueoueoue67',
-      mail: 'lafamille@letsgo.org',
-      name: 'darksasuke',
-      createdAt: '2023-03-25T13:20:24.579Z',
+      login: "Alfredus",
+      password: "oueoueoue67",
+      mail: "lafamille@letsgo.org",
+      name: "darksasuke",
+      createdAt: "2023-03-25T13:20:24.579Z",
     },
     song: {
       id: 3,
-      name: 'A blessing and a curse',
-      album: 'A blessing and a curse',
-      artist: 'Here come the mummies',
-      thumbnail: 'http://toto.png',
-      createdAt: '2023-03-25T13:20:24.579Z',
+      name: "A blessing and a curse",
+      album: "A blessing and a curse",
+      artist: "Here come the mummies",
+      thumbnail: "http://toto.png",
+      createdAt: "2023-03-25T13:20:24.579Z",
     },
     results: [
       {
@@ -43,29 +43,29 @@ const doc = {
         songId: 1,
         votesNb: null,
         submitterId: null,
-        createdAt: '2023-03-28T16:33:53.772Z',
+        createdAt: "2023-03-28T16:33:53.772Z",
         song: {
           id: 1,
-          name: 'A Blessing And A Curse',
-          album: 'Future Worlds',
-          artist: 'Atomica Music',
+          name: "A Blessing And A Curse",
+          album: "Future Worlds",
+          artist: "Atomica Music",
           thumbnail:
-            'https://i.scdn.co/image/ab67616d00004851ea6d794f7e1c9b2fb8b73890',
-          createdAt: '2023-03-26T11:35:29.102Z',
+            "https://i.scdn.co/image/ab67616d00004851ea6d794f7e1c9b2fb8b73890",
+          createdAt: "2023-03-26T11:35:29.102Z",
         },
       },
     ],
   },
 };
-const outputFile = 'swagger_output.json';
+const outputFile = "swagger_output.json";
 const endpointFiles = [
-  'src/routes/users.js',
-  'src/routes/router.js',
-  'src/routes/songs.js',
-  'src/routes/votes.js',
-  'src/routes/playlistSongs.js',
-  'src/routes/playlists.js',
-  'src/routes/authentication.js',
+  "src/backend/routes/users.js",
+  "src/backend/routes/router.js",
+  "src/backend/routes/songs.js",
+  "src/backend/routes/votes.js",
+  "src/backend/routes/playlistSongs.js",
+  "src/backend/routes/playlists.js",
+  "src/backend/routes/authentication.js",
 ];
 
 swaggerAutogen(outputFile, endpointFiles, doc);
