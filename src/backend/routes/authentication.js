ef1a44e7c09c5304874d9e8f13dfdd6ae73d58fc
tibo <dornthibault@gmail.com>
2023-04-23 13:04:39 +0200

    Init frontend repo

--- src/backend/routes/authentication.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const users = require('../controllers/users');
-
-router.post('/auth', users.getToken);
-module.exports = router;
--- src/backend/server.js
+++
@@ -1,9 +0,0 @@
-// require('mandatoryenv').load(['PORT']);
-const dotenv = require('dotenv');
-dotenv.config();
-const { PORT } = process.env;
-
-// Instantiate an Express Application
-const app = require('./app');
-// Open Server on selected Port
-app.listen(PORT, () => console.info('Server listening on port ', PORT));
