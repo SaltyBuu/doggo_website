4831b28e8437772e67b4ba03914f20625560554d
tibo <dornthibault@gmail.com>
2023-04-23 12:39:07 +0200

    Add default port

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,9 +1,9 @@
 // require('mandatoryenv').load(['PORT']);
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const { PORT } = process.env;
+const { PORT } = process.env || 3000;
 
 // Instantiate an Express Application
-const app = require('./app');
+const app = require("./app");
 // Open Server on selected Port
-app.listen(PORT, () => console.info('Server listening on port ', PORT));
+app.listen(PORT, () => console.info("Server listening on port ", PORT));
