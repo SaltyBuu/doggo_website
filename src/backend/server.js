d2e8c9ce46deeead24daaca802c03c6a25e896d2
tibo <dornthibault@gmail.com>
2023-04-23 11:40:29 +0200

    Setup to deploy on heroku

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,17 +1,9 @@
 // require('mandatoryenv').load(['PORT']);
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
 const { PORT } = process.env;
 
 // Instantiate an Express Application
-const app = require("./app");
+const app = require('./app');
 // Open Server on selected Port
-if (typeof PhusionPassenger !== "undefined") {
-  PhusionPassenger.configure({ autoInstall: false });
-}
-
-if (typeof PhusionPassenger !== "undefined") {
-  app.listen("passenger");
-} else {
-  app.listen(3000);
-}
+app.listen(PORT, () => console.info('Server listening on port ', PORT));
