f5f900f76420c360b34d91f4973162f59815c36f
tibo <dornthibault@gmail.com>
2023-05-18 18:59:08 +0200

    Add logs

--- src/backend/middlewares/validation.js
+++ src/backend/middlewares/validation.js
@@ -16,27 +16,22 @@ module.exports = {
     if (has(req.headers, ['x-access-token'])) {
       const token = req.headers['x-access-token'];
       // console.log('Token reçu:', token);
-      jwt.verify(
-        token,
-        TOKENSECRET,
-        { algorithm: 'HS256' },
-        function (err, decoded) {
-          if (err) {
-            // console.log('JsonWebToken error:', err);
-            res.status(403).json({ message: 'Expired token.' });
+      jwt.verify(token, TOKENSECRET, { algorithm: 'HS256' }, function (err, decoded) {
+        if (err) {
+          // console.log('JsonWebToken error:', err);
+          res.status(403).json({ message: 'Expired token.' });
+        } else {
+          // console.log('exp:', decoded.exp);
+          // console.log('exp*1000:', decoded.exp);
+          if (Date.now() >= decoded.exp) {
+            console.log('valid token', decoded.login, decoded.userId);
+            next();
           } else {
-            // console.log('exp:', decoded.exp);
-            // console.log('exp*1000:', decoded.exp);
-            if (Date.now() >= decoded.exp) {
-              console.log('valid token');
-              next();
-            } else {
-              console.log('expired token !!');
-              next(new CodeError('Expired token', 403));
-            }
+            console.log('expired token !!');
+            next(new CodeError('Expired token', 403));
           }
         }
-      );
+      });
     } else {
       next(new CodeError('Forbidden', 403));
     }
@@ -45,27 +40,22 @@ module.exports = {
     if (has(req.headers, ['x-access-token'])) {
       const token = req.headers['x-access-token'];
       // console.log('Token reçu:', token);
-      jwt.verify(
-        token,
-        TOKENSECRET,
-        { algorithm: 'HS256' },
-        function (err, decoded) {
-          if (err) {
-            // console.log('JsonWebToken error:', err);
-            res.status(403).json({ message: 'Expired token.' });
+      jwt.verify(token, TOKENSECRET, { algorithm: 'HS256' }, function (err, decoded) {
+        if (err) {
+          // console.log('JsonWebToken error:', err);
+          res.status(403).json({ message: 'Expired token.' });
+        } else {
+          // console.log('exp:', decoded.exp);
+          // console.log('exp*1000:', decoded.exp);
+          if (decoded.isAdmin) {
+            console.log('Admin user', decoded.login);
+            next();
           } else {
-            // console.log('exp:', decoded.exp);
-            // console.log('exp*1000:', decoded.exp);
-            if (decoded.isAdmin) {
-              console.log('Admin user', decoded.login);
-              next();
-            } else {
-              res.status(403).json({ message: 'Unauthorized user.' });
-              next(new Error('Unauthorized user : ' + decoded.login));
-            }
+            res.status(403).json({ message: 'Unauthorized user.' });
+            next(new Error('Unauthorized user : ' + decoded.login));
           }
         }
-      );
+      });
     } else {
       next(new CodeError('Forbidden', 403));
     }
